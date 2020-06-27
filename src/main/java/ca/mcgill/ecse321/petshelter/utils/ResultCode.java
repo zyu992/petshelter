cf34051dae355e36a3a99b8a5afd04bd68e71e08
Junjie Yang <junjie.yang2@mail.mcgill.ca>
2020-06-27 22:26:15 +0800

    rm unused imports, starting on testcases

--- src/main/java/ca/mcgill/ecse321/petshelter/utils/ResultCode.java
+++ src/main/java/ca/mcgill/ecse321/petshelter/utils/ResultCode.java
@@ -1,12 +1,12 @@
 package ca.mcgill.ecse321.petshelter.utils;
 
 public enum ResultCode {
-    SUCCESS(true, 10000, "Succeeded"),
-    FAIL(false, 10001, "Failed"),
-    UNAUTHENTICATED(false, 10002, "Not logged in"),
-    UNAUTHORIZED(false, 10003, "Unauthorized"),
-    SERVER_ERROR(false, 99999, "Server error"),
-    USERNAME_OR_PASSWORD_ERROR(false, 20000, "Credentials do not match with our records");
+    SUCCESS(true, 202, "Succeeded"),
+    FAIL(false, 200, "Failed"),
+    UNAUTHENTICATED(false, 401, "Not logged in"),
+    UNAUTHORIZED(false, 401, "Unauthorized"),
+    SERVER_ERROR(false, 503, "Server error"),
+    USERNAME_OR_PASSWORD_ERROR(false, 401, "Credentials do not match with our records");
 
     boolean success;
     int code;
---
+++ src/test/java/ca/mcgill/ecse321/petshelter/util/TestUtil.java
@@ -0,0 +1,8 @@
+package ca.mcgill.ecse321.petshelter.util;
+
+import ca.mcgill.ecse321.petshelter.entity.*;
+
+public class TestUtil {
+//    public static final User testUser = createUser();
+
+}
